2aec7c14cae21cdb68ccf247ad36a47315a6d6ba
Pavel Voronin <Pavel.Voronin@dataart.com>
2013-12-03 15:55:21 +0200

    APSF-110 Branding and Skinning server-side architecture

--- factory.js
+++ factory.js
@@ -4,5 +4,6 @@ module.exports = {
     auth: require('./lib/auth/authentication'),
     authMock: require('./lib/authMock/authentication'),
     serviceProxy: require('./lib/serviceProxy'),
-    services: require('./lib/services')
+    services: require('./lib/services'),
+    theme: require('./lib/theme')
 };
