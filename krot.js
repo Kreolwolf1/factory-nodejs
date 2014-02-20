f1cb22e1cce92ac8209d76c32a8aac1e8942ec8f
Eugene.Tsypkin <eugene.tsypkin@dataart.com>
2014-02-20 13:23:13 +0200

    add validation to main file

--- krot.js
+++ krot.js
@@ -4,5 +4,6 @@ module.exports = {
     auth: require('./lib/auth/authentication'),
     authMock: require('./lib/authMock/authentication'),
     serviceProxy: require('./lib/serviceProxy/serviceProxy'),
-    services: require('./lib/services/services')
+    services: require('./lib/services/services'),
+    validation: require('./lib/validation/validation')
 };
