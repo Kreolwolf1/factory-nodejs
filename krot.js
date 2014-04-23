e8b6584575b16881c376a58a700609a8e4ff832c
Eugene.Tsypkin <eugene.tsypkin@dataart.com>
2014-04-23 12:13:21 +0300

    DEVP-2523 add password mock authentication

--- krot.js
+++ krot.js
@@ -3,6 +3,7 @@
 module.exports = {
     auth: require('./lib/auth/authentication'),
     authMock: require('./lib/authMock/authentication'),
+    passwordAuthMock: require('./lib/passwordAuthMock/authentication'),
     serviceProxy: require('./lib/serviceProxy/serviceProxy'),
     services: require('./lib/services/services'),
     validation: require('./lib/validation/validation')
